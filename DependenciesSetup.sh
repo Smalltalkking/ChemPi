918c8797c9fbcc4e33e82ec2bcb522aca08736c5
TobiasFP <tobi.fp@gmail.com>
2016-08-13 00:39:45 +0200

    Create DependenciesSetup.sh

---
+++ DependenciesSetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This project is running on Ubuntu Mate for Raspberry pi, but will be written in 
+#python and php, so it should be easy to setup on different environments, and even different units, 
+#like intel based computers, and running parts on arduino, etc.
+
+sudo apt-get install pip3 
+
+sudo pip3 apt-get install
