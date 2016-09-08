156c4ec815b767ba8df871bd36093e95ddd37423
TobiasFP <tobi.fp@gmail.com>
2016-09-08 21:57:24 +0200

    Update DependenciesSetup.sh

--- DependenciesSetup.sh
+++ DependenciesSetup.sh
@@ -8,10 +8,11 @@ cd workspace
 mkdir chempi
 cd chempi
 #Standard install without Big Algae
-sudo apt-get install pip3 apache2 php mariadb-server git
+sudo apt-get install apache2 php mariadb-server git 
 
 #Big Algae dependencies
-sudo apt-get install OpenCV piexif GPy numpy scipy matplotlib
+sudo apt-get install python-opencv libcv-dev python-matplotlib python-scipy python-numpy python-pip
+sudo pip install piexif GPy  
 git clone --recursive https://github.com/Big-Algae-Open-Experiment/computational_writeup.git
 git clone --recursive https://github.com/Big-Algae-Open-Experiment/bigalgae.git
 git clone --recursive https://github.com/TobiasFP/ChemPi.git
