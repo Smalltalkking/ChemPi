d61c52f1d23476938ac1adf2dcf8c1ff12072523
TobiasFP <tobi.fp@gmail.com>
2016-09-09 07:42:32 +0200

    Update WebConfigPost.php

--- html/WebConfigPost.php
+++ html/WebConfigPost.php
@@ -14,7 +14,15 @@ $Interval = $_POST["Interval"];
 
 // Open the file 
 $file = 'config.txt';
-$configText = "Temperature = " . $Temperature . "\n" . "Light = " . $Light . "\n" . "Interval = " . $Interval . "\n";
+$configText = 
+"Temperature = " . $_POST["Temperature"] . "\n TempGPIO = " . $_POST["TempGPIO"] .
+"\n" . "Light = " . $_POST["Light"] . "\n" . "LightGPIO = " . $_POST["LightGPIO"] . 
+"\n" . "pH = " . $_POST["pH"] . "\n" . "pHGPIO = " . $_POST["pHGPIO"] . 
+"\n" . "CarbonDioxide = " . $_POST["CarbonDioxide"] . "\n" . "CarbonDioxideGPIO = " . $_POST["CarbonDioxideGPIO"] . 
+"\n" . "Oxygen = " . $_POST["Oxygen"] . "\n" . "OxygenGPIO = " . $_POST["OxygenGPIO"] . 
+"\n" . "PiCam = " . $_POST["PiCam"] . "\n" . "CamRepInterval = " . $_POST["CamRepInterval"] . 
+"\n" . "Interval = " . $_POST["Interval"] . 
+"\n" . "";
 // Write the contents back to the file
 file_put_contents($file, $configText);
 
