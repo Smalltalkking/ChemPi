eeefc988dd561ff1e5d5eb4735d16e1b0a397ac5
TobiasFP <tobi.fp@gmail.com>
2016-09-10 14:49:48 +0200

    Update WebConfig.php

--- html/WebConfig.php
+++ html/WebConfig.php
@@ -15,22 +15,23 @@
  
  <form action="WebConfigPost.php" method="post">
    <fieldset>
-   <p> <label for="Temperature" >Temperature: </label> <input type="checkbox" name="Temperature" value="Temperature" id="Temperature">  </p>
+   <p> <label for="Temperature" >Temperature: </label> <input type="checkbox" name="Temperature" id="Temperature">  </p>
    <p> <label for="TempGPIO" > GPIO number: </label><input type="number" name="TempGPIO" id="TempGPIO" min="0" max="50" step="1" value="3">   <br /> </p>
-   <p> <label for="Light" > Light: </label><input type="checkbox" name="Light" id="Light" value="Light">  </p>
+   <p> <label for="Light" > Light: </label><input type="checkbox" name="Light" id="Light" >  </p>
    <p> <label for="LightGPIO" > GPIO number: </label><input type="number" name="LightGPIO" id="LightGPIO" min="0" max="50" step="1" value="21">  <br /></p>
-   <p> <label for="pH" >pH: </label><input type="checkbox" name="pH" id="pH" value="pH"> </p>
+   <p> <label for="pH" >pH: </label><input type="checkbox" name="pH" id="pH"> </p>
    <p> <label for="pHGPIO" >GPIO number: </label><input type="number" name="pHGPIO" id="pHGPIO" min="0" max="50" step="1" value="27"> <br /></p>
-   <p> <label for="CarbonDioxide" >CarbonDioxide: </label><input type="checkbox" name="CarbonDioxide"id="CarbonDioxide" value="CarbonDioxide">  </p>
+   <p> <label for="CarbonDioxide" >CarbonDioxide: </label><input type="checkbox" name="CarbonDioxide"id="CarbonDioxide" >  </p>
    <p> <label for="CarbonDioxideGPIO" >GPIO number: </label><input type="number" name="CarbonDioxideGPIO"id="CarbonDioxideGPIO" min="0" max="50" step="1" value="3"> <br /> </p>
-   <p> <label for="Oxygen" >Oxygen: </label><input type="checkbox" name="Oxygen" id="Oxygen" value="Oxygen">  </p>
+   <p> <label for="Oxygen" >Oxygen: </label><input type="checkbox" name="Oxygen" id="Oxygen">  </p>
    <p> <label for="OxygenGPIO" >GPIO number: </label><input type="number" name="OxygenGPIO" id ="OxygenGPIO"  min="0" max="50" step="1" value="3">  <br /></p>
 
-   <p> <label for="Interval" >Update interval [sec]: </label><input type="number" name="Interval" id="Interval" min="0" max="100000" step="10" value="3"> <br /> <br /></p>
+   <p> <label for="Interval" >Update interval [sec]: </label><input type="number" name="Interval" id="Interval" min="0" max="100000" step="1" value="3"> <br /> <br /></p>
 
-   <p> <label for="PiCam" >Pi Camera: </label><input type="checkbox" name="PiCam" id="PiCam" value="PiCam"> </p>
+   <p> <label for="PiCam" >Pi Camera: </label><input type="checkbox" name="PiCam" id="PiCam"> </p>
 
    <p><label for="CamRepInterval" > Camera capture interval [sec]: </label><input type="number" name="CamRepInterval" id="CamRepInterval" min="0" max="100000" step="1" value="3600"> </p><br />
+  <input type="submit" value="Submit">
    </fieldset>
    </form>
    </body>
