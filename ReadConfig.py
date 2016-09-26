e4af5db7aacf53a232bfe1bbd73f34be233cf8a9
TobiasFP <tobi.fp@gmail.com>
2016-09-26 12:58:55 +0200

    Delete ReadConfig.py

--- ReadConfig.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#Imports:
-import configparser
-
-config = configparser.ConfigParser()
-config.read('html/config.txt')
-print(config['Config']['Temperature'])
-print(config['Config']['TempGPIO'])
-print(config['Config']['PiCam'])
